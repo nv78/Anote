a4c2f711931e497083389e1a85ce229e07e4d1fa
Liang Zhang <87407915+Whiteii@users.noreply.github.com>
2024-02-11 14:50:58 -0500

    bert fting fix

--- Anote-API/predict/finetune.py
+++ Anote-API/predict/finetune.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
 
-#Components Needed For FTING BERT
+# FTING BERT
 
 class DataLoader(Dataset):
     def __init__(self, encodings, labels):
@@ -45,18 +45,14 @@ def compute_metrics(pred):
         'Recall': recall
     }
 
-
-
-
-
-
 training_args = TrainingArguments(
+    output_dir='./BERTModel2',
     do_train=True,
     do_eval=True,
-    num_train_epochs=7,  # Reduced number of epochs.
-    per_device_train_batch_size=10,  # Reduced batch size for training.
-    per_device_eval_batch_size=35,  # Reduced batch size for evaluation.
-    warmup_steps=100,
+    num_train_epochs=1,  # Reduced number of epochs.
+    per_device_train_batch_size=5,  # Reduced batch size for training.
+    per_device_eval_batch_size=20,  # Reduced batch size for evaluation.
+    warmup_steps=50,
     weight_decay=0.01,
     logging_strategy='steps',
     logging_dir='./multi-class-logs',
@@ -64,9 +60,8 @@ training_args = TrainingArguments(
     evaluation_strategy="steps",
     eval_steps=50,
     save_strategy="steps",
-    #fp16=True,  # Enable mixed precision training.
+    fp16=True,  # Enable mixed precision training.
 )
-
 
 
 ########################################################################################################################################################
@@ -74,26 +69,68 @@ training_args = TrainingArguments(
 
 
 
-class BERT:
-    def __init__(self,train_dataset,eval_dataset):
-        """
-        Initializes a BERT-based sequence classifier.
+class FT_BERT:
+    def __init__(self,train_dataset,eval_dataset,num_labels):
+        """ Initializes a BERT-based sequence classifier.
+
         Args:
-            train_dataset (pandas.DataFrame): Training dataset containing 'text' and 'label' columns.
-            eval_dataset (pandas.DataFrame): Evaluation dataset containing 'text' and 'label' columns.
-        """
+            train_dataset (pandas.DataFrame): Training dataset containing 'text' and 'label' columns. 
+                The 'label' column must contain string values.
+            eval_dataset (pandas.DataFrame): Evaluation dataset containing 'text' and 'label' columns. 
+                The 'label' column must contain string values.
+
+        Example:
+            ```python
+            # Initialize FTING BERT classifier
+            BERT = Fine_Tuning_BERT(train_df, test_df, num_labels=5)
+
+            # Texts for prediction
+            texts =  ["this product is trash"]
+
+            # Once BERT finishes fine-tuning, you can call the .prediction method with an array of texts
+            predicted_class = BERT.prediction(texts=texts)
+            print(predicted_class) """
+
+
         self.tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased", max_length=512)
 
         self.train_encodings = self.tokenizer(train_dataset['text'].to_list(), truncation=True, padding=True)
         self.eval_encodings = self.tokenizer(eval_dataset['text'].to_list(), truncation=True, padding=True)
 
-        self.train_labels = train_dataset.label.to_list()
-        self.eval_labels = eval_dataset.label.to_list()
+        self.train_labels = train_dataset.labels.to_list()
+        self.eval_labels = eval_dataset.labels.to_list()
 
-        self.id_to_label = {idx: label for idx, label in enumerate(set(self.train_labels))}
-        self.label_to_id = {label: idx for idx, label in enumerate(set(self.eval_labels))}
 
-        self.model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=3, id2label =  self.id_to_label, label2id =  self.label_to_id)
+        self.train_labels = []
+        for label in train_dataset.labels.to_list():
+          if isinstance(label, str):
+            self.train_labels.append(label)
+          else:
+            raise ValueError("All labels in train_dataset.labels must be strings.")
+
+        self.eval_labels = []
+        for label in eval_dataset.labels.to_list():
+          if isinstance(label, str):
+            self.eval_labels.append(label)
+          else:
+            raise ValueError("All labels in eval_dataset.labels must be strings.")
+
+
+        self.all_labels = self.eval_labels + self.train_labels
+
+        print(self.all_labels)
+
+        self.id_to_label = {idx: label for idx, label in enumerate(set(self.all_labels))}
+        self.label_to_id = {label: idx for idx, label in self.id_to_label.items()}
+
+
+        self.train_labels = [self.label_to_id[label] for label in self.train_labels]
+
+        self.eval_labels = [self.label_to_id[label] for label in self.eval_labels]
+
+        self.model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels = 5 , id2label =  self.id_to_label, label2id =  self.label_to_id)
+
+
 
         self.train_dataloader = DataLoader(self.train_encodings,self.train_labels)
         self.eval_dataloader = DataLoader(self.eval_encodings,self.eval_labels)
@@ -118,7 +155,7 @@ class BERT:
 
         self.metrics = self.trainer.evaluate()
 
-        return self.ft_model, self.metrics
+        return self.model
 
 
     def model_evaluation(self):
@@ -131,18 +168,26 @@ class BERT:
 
 
     def prediction(self,texts):
+        # Text must be an array 
+
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
         self.inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
 
+        self.inputs.to(self.device)
+
 
         with torch.no_grad():
-            self.outputs = self.fine_tune_bert(**self.inputs)
+            self.outputs = self.model(**self.inputs)
 
         # Get predicted label IDs
         predicted_label_ids = torch.argmax(self.outputs.logits, dim=1).tolist()
 
-        # Convert predicted label IDs to labels
-        predicted_labels = [self.outputs.config.id2label[label_id] for label_id in predicted_label_ids]
 
-        return predicted_labels
+
+        return predicted_label_ids
+
+
+
