4c0b0f968dca6b652a02e84e8e5dd6f860a2fc12
Natan Vidra <vidranatan@gmail.com>
2024-02-12 16:00:55 -0500

    updated

---
+++ Anote-API/anote.py
@@ -0,0 +1,19 @@
+
+
+def upload():
+    """
+    insert docstring
+    """
+    pass
+
+def evaluate():
+    """
+    insert docstring
+    """
+    pass
+
+def predict():
+    """
+    insert docstring
+    """
+    pass
