95769b6648c97b6846387ab30fe1ab8b9f60df84
Natan Vidra <vidranatan@gmail.com>
2024-02-12 14:57:17 -0500

    updated

--- Anote-API/testing/run_all_tests.py
+++ Anote-API/testing/run_all_tests.py
@@ -2,9 +2,13 @@ import pytest
 
 def run_all_tests():
     # Run your pytest test cases here
-    pytest.main(['./test_functions/test_upload.py'])  # Replace 'test_evaluate.py' with your test file names
-    pytest.main(['./test_functions/test_evaluate.py'])  # Add more test files as needed
-    pytest.main(['./test_functions/test_sentiment_analysis.py'])  # Add more test files as needed
+    pytest.main(['./test_functions/test_upload.py'])
+    pytest.main(['./test_functions/test_evaluate.py'])
+    pytest.main(['./test_functions/test_classify.py'])
+    pytest.main(['./test_functions/test_ner.py'])
+    pytest.main(['./test_functions/test_answer.py'])
+    pytest.main(['./test_functions/test_rag.py'])
+    pytest.main(['./test_functions/test_finetuning.py'])
 
 if __name__ == "__main__":
     run_all_tests()
