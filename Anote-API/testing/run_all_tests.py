86cb832a8f3938378bc45aa8e81ba61cf3f4e982
Liang Zhang <87407915+Whiteii@users.noreply.github.com>
2024-02-14 22:08:29 -0500

    1

--- Anote-API/testing/run_all_tests.py
+++ Anote-API/testing/run_all_tests.py
@@ -1,14 +1,23 @@
 import pytest
+import os 
+import sys
+
+current_dir = os.path.dirname(__file__)
+grandparent_dir = os.path.abspath(os.path.join(current_dir, '../../'))
+sys.path.append(grandparent_dir)
+print(grandparent_dir)
+
+
 
 def run_all_tests():
     # Run your pytest test cases here
-    pytest.main(['./test_functions/test_upload.py'])
-    pytest.main(['./test_functions/test_evaluate.py'])
+
+    #pytest.main(['./test_functions/test_upload.py'])
+    #pytest.main(['./test_functions/test_evaluate.py'])
+    #pytest.main(['./test_functions/test_ner.py'])
     pytest.main(['./test_functions/test_classify.py'])
-    pytest.main(['./test_functions/test_ner.py'])
-    pytest.main(['./test_functions/test_answer.py'])
-    pytest.main(['./test_functions/test_rag.py'])
-    pytest.main(['./test_functions/test_finetuning.py'])
+    #pytest.main(['./test_functions/test_rag.py'])
+    #pytest.main(['./test_functions/test_finetuning.py'])
 
 if __name__ == "__main__":
     run_all_tests()
