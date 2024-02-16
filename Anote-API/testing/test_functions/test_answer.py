9c5241fc52861c85ffb9bc413a28fdb6e138763f
Natan Vidra <vidranatan@gmail.com>
2024-02-16 11:44:33 -0500

    updated

--- Anote-API/testing/test_functions/test_answer.py
+++ Anote-API/testing/test_functions/test_answer.py
@@ -11,7 +11,7 @@ sys.path.append(grandparent_dir)
 from unittest.mock import patch, MagicMock
 from util.predictions.answer import generate_answers
 
-def question_answering_private():
+def test_question_answering_private():
     question = "What is the capital of France?"
     context = "The capital of France is Paris."
 
@@ -24,13 +24,33 @@ def question_answering_private():
     print(response)
     assert response is not None
 
-# # Test for Custom QA Model
-# @patch("predict.answer.pipeline")
-# def test_generate_answers_custom_qa(mock_pipeline, setup_question_answering):
-#     question, context = setup_question_answering
 
-#     # Mocking the pipeline's return value
-#     mock_pipeline.return_value = MagicMock(return_value={"answer": "Paris"})
 
-#     answer = generate_answers(question, context, model_type="custom_qa")
-#     assert "Paris" in answer, "The function should return Paris as the answer"
+def test_question_answering_claude():
+    question = "What is the capital of France?"
+    context = "The capital of France is Paris."
+
+    response = generate_answers(
+        question=question,
+        context=context,
+        model_type="claude",
+        private=True
+    )
+
+    print(response)
+    assert response is not None
+
+
+def test_question_answering_gpt():
+    question = "What is the capital of France?"
+    context = "The capital of France is Paris."
+
+    response = generate_answers(
+        question=question,
+        context=context,
+        model_type="gpt",
+        private=True
+    )
+
+    print(response)
+    assert response is not None
